74421ac2c672cfe9d59490e71e9bd37bcf8f2239
Tibor Benke <ihrwein@gmail.com>
2015-07-05 21:05:13 +0200

    rust: proxy clone calls
    A new, rust_parser_clone()  is registered as the LogPipe's clone method so
    a RustParser can be used in multiple logpaths.
    
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- modules/rust/parser.c
+++ modules/rust/parser.c
@@ -6,6 +6,9 @@ typedef struct _ParserRust {
   struct RustParserProxy* rust_object;
 } ParserRust;
 
+static LogPipe*
+rust_parser_clone(LogPipe *s);
+
 gboolean
 rust_parser_process(LogParser *s, LogMessage **pmsg, const LogPathOptions *path_options, const gchar *input, gsize input_len)
 {
@@ -28,7 +31,6 @@ rust_parser_init(LogPipe *s)
 {
   ParserRust *self = (ParserRust *) s;
 
-  log_parser_init(&self->super.super);
   return !!rust_parser_proxy_init(self->rust_object);
 }
 
@@ -39,6 +41,33 @@ rust_parser_free(LogPipe *s)
 
   rust_parser_proxy_free(self->rust_object);
   log_parser_free_method(s);
+}
+
+static void
+__setup_callback_methods(ParserRust *self)
+{
+  self->super.process = rust_parser_process;
+  self->super.super.free_fn = rust_parser_free;
+  self->super.super.clone = rust_parser_clone;
+  self->super.super.init = rust_parser_init;
+}
+
+static LogPipe*
+rust_parser_clone(LogPipe *s)
+{
+  ParserRust *self = (ParserRust*) s;
+  ParserRust *cloned;
+  
+  cloned = (ParserRust*) g_new0(ParserRust, 1);
+  log_parser_init_instance(&cloned->super, s->cfg);
+  cloned->rust_object = rust_parser_proxy_clone(self->rust_object);
+
+  if (!cloned->rust_object)
+    return NULL;
+
+  __setup_callback_methods(cloned);
+  
+  return &cloned->super.super; 
 }
 
 LogParser*
@@ -52,9 +81,7 @@ rust_parser_new(const gchar *name, GlobalConfig *cfg)
   if (!self->rust_object)
     return NULL;
 
-  self->super.process = rust_parser_process;
-  self->super.super.free_fn = rust_parser_free;
-  self->super.super.init = rust_parser_init;
+  __setup_callback_methods(self);
 
   return &self->super;
 }
